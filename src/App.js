b28c38d7c8b6ceccb520ed5a92d00becb7df0cbf
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-05-10 12:30:50 +0100

    interceptor

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Title from './components/Title';
+import Setup from './examples/5-custom-instance'
+import './axios/global'
+function App() {
+  return (
+    <main>
+      <Title />
+      <Setup/>
+    </main>
+  );
+}
+
+export default App;
