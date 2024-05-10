ae976f7237c67cb34c8e1ce2cb72f2298404d088
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-05-10 15:42:51 +0100

    CHANGES

--- src/examples/1-first-request.js
+++ src/examples/1-first-request.js
@@ -1,27 +1,66 @@
-import { useEffect } from 'react';
 import axios from 'axios'
+import { useEffect,useState } from 'react';
+
+
+
+
 
 // limit, if 429 wait for 15 min and try again
-const url = 'https://course-api.com/react-store-products';
+// const url = 'https://course-api.com/react-store-products';
 
-const FirstRequest =   () => {
-  const fetchData=async () =>{
-    try{
-      const response= await axios(url);
-      const data=response.data;
-      console.log(data);
-  
-    }catch(e){
-      console.log(e.response);
-  
+const FirstRequest =  async () => {
+  const [word,setWord]=useState('');
+
+  const options = {
+    method: 'GET',
+    url: 'https://mashape-community-urban-dictionary.p.rapidapi.com/define',
+    params: {term: word},
+    headers: {
+      'X-RapidAPI-Key': '725fb5b942msh802001ec5ca8863p134943jsnec754daad71f',
+      'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com'
     }
+  };
   
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
   }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  return(
+<form onSubmit={FirstRequest}>
+  <input type='hidden '  name='chuks' value={word}  onChange={(e)=>{setWord(e.target.value)}}/>
 
-  return <h2 className='text-center'>first request</h2>;
+</form>
+  );
+
+
+
+
+
+
+
+
+
+
+  // const fetchData=async () =>{
+  //   try{
+  //     const response= await axios(url);
+  //     const data=response.data;
+  //     console.log(data);
+  
+  //   }catch(e){
+  //     console.log(e.response);
+  
+  //   }
+  
+  // }
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  // return <h2 className='text-center'>first request</h2>;
 };
 export default FirstRequest;
